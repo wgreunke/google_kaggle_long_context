6641ec2d1bc9d24030afffc4226d0c3f5cbc2988
Ward Greunke <wgreunke@gmail.com>
2024-11-22 12:52:33 -0800

    Cleaned up, added image, getting ready for adding llm call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,79 +1,27 @@
 import streamlit as st
 import pandas as pd
 
-
-# Add custom CSS
-st.markdown(
-    """
-    <style>
-    .button {
-        display: inline-block;
-        margin: 2px; /* Reduce spacing between buttons */
-        background-color: transparent; /* Remove button background */
-        border: none; /* Remove border */
-        color: blue; /* Set text color to blue */
-        font-size: 16px; /* Adjust text size if needed */
-        cursor: pointer; /* Make it look clickable */
-    }
-    .button:hover {
-        text-decoration: underline; /* Add hover effect */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+#This is the function that calls the llm and generates the code.
+def generate_code_from_video(video_file):
+    llm_response="test"
+    return llm_response
 
 
-st.title("Code from Video")
 
-st.write("Stop pressing pause to copy code from video!")
+#********************* UI ***************************
+st.title("Video to Code")
 
+st.header("Stop pressing pause to copy code from video!")
 
-url="www.yahoo.com"
 st.markdown("This project was part of the Google Gemini Long Context [Kaggle Competition](https://www.kaggle.com/competitions/gemini-long-context/overview)", unsafe_allow_html=True)
 
-videos_list=[
-    {"Title":"First Video","URL":"https://www.youtube.com/shorts/N6p3c-CH8ZI","ID":1},
-    {"Title":"Second Video","URL":"https://www.youtube.com/shorts/lUBPD2bWodA","ID":2}
-]
+left_co,middle_co, cent_co,last_co = st.columns(4)
+with middle_co:
+    st.image("vtc.png",width=300)
 
-video_df=pd.DataFrame(videos_list)
+st.markdown("Upload an mp4 tutorial video then press generate code.  Google long-context will do the rest!" )
 
-current_id=""
-
-st.write("Choose a video to extract code code:")
-#Show a list of videos
-for video in videos_list:
-    if st.button(video["Title"],):
-        current_id=video["ID"]
-    #st.write(video["Title"])
-    #st.write(video["URL"])
-
-#Given the id grab the row for the video that has the data
-video_row=video_df[video_df["ID"]==current_id]
-st.write(current_id)
-
-# ************* File Uploader ****************
-
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    # To read file as bytes:
-    bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
-
-    # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-#*********** Text File upload **************
+#*********** Video File upload **************
 uploaded_file=None
 uploaded_video=st.file_uploader("Choose a video",type=["mp4"])
 if uploaded_video is not None:
@@ -81,13 +29,8 @@ if uploaded_video is not None:
     #st.video(uploaded_video)
 
 
-
-
-
-
+#Use columns to control the size of the video
 col1,col2,col3=st.columns([3,3,3])
-
-
 
 with col2:
     
@@ -95,8 +38,15 @@ with col2:
     if uploaded_video is not None:
         st.video(uploaded_video)
 
-        st.button("Generate Code")
+        if st.button("Generate Code"):
+            st.write(generate_code_from_video(uploaded_video))
+        
 
 
+
+
+
+st.markdown("Written by [Ward Greunke](https://www.linkedin.com/in/wgreunke/)", unsafe_allow_html=True) 
+st.markdown("Thanks to ?????? for the starter notebook. [Kaggle Competition](https://www.kaggle.com/competitions/gemini-long-context/overview)", unsafe_allow_html=True)
 
 
